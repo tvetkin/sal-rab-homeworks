8f156d5d355bad26203cda6ecb8fcf849106f717
tvetkin <kovalchuk65@yandex.ru>
2022-05-10 12:55:05 +0300

    repait codestyle ht2

--- task_2/solution.js
+++ task_2/solution.js
@@ -14,11 +14,15 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    productsSum>=freeShippingMinSum) {shippingSum=0;};
+    productsSum>=freeShippingMinSum) {
+        shippingSum=0;
+    };
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum>0&&productsSum<freeShippingMinSum){shippingSum=shippingPrice;};
+    if (productsSum>0&&productsSum<freeShippingMinSum){
+        shippingSum=shippingPrice;
+    };
 
     // Конец решения задания №2.1.
 
@@ -38,8 +42,12 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-    if (productsSum>=discountMinSum){discountSum=discountPart/100*productsSum;}
-    else{discountSum=0};
+    if (productsSum>=discountMinSum){
+        discountSum=discountPart/100*productsSum;
+    }
+    else{
+        discountSum=0
+    };
 
     // Конец решения задания №2.2.
 
