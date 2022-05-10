531ee79dc75e20731de0856f81bb8796984a751c
tvetkin <kovalchuk65@yandex.ru>
2022-05-10 13:04:36 +0300

    test comment 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -20,6 +20,7 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+   
     if (productsSum>0&&productsSum<freeShippingMinSum){
         shippingSum=shippingPrice;
     };
