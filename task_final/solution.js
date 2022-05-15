c30f2a37f3ce5aa0ed8fb8c1d61cc53fe59d9a26
tvetkin <kovalchuk65@yandex.ru>
2022-05-15 21:00:49 +0300

    final v1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,27 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {
+        client: '', 
+        goods: [
+            {title: this.title},
+            {count: count},
+        ], 
+        order: {
+            address:'', 
+            sum: sum,
+        }
+    }
+    data.client = name + ' ' + phone;
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    for (let i = 0; i < countOfGoods; i ++) {
+        data.goods.push({"title": goods[i].title, "count": goods[i].count});
+    }  
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify(data); //???let jsonData = JSON.stringify({data:data});
 
     return jsonData;
-}
+};
